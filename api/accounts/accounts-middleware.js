e4190b9b50007456ad190ccb4798287202e19355
Kade Griffith <kadeg95@gmail.com>
2021-10-04 14:32:28 -0600

    final commit. MVP is met.

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -9,7 +9,7 @@ exports.checkAccountPayload = (req, res, next) => {
   } else if(name.trim().length < 3 || name.trim().length > 100){
     next({status:400, message: "name of account must be between 3 and 100"})
   } else if(typeof budget !== 'number'){
-    next({status: 400, message: "budget of account must be a number"})
+    next({status:400, message: "budget of account must be a number"})
   } else if(budget < 0 || budget > 1000000){
     next({status: 400, message: "budget of account is too large or too small"})
   } else if( name && typeof budget === 'number'){
